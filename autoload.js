a0b2ca6fec9393f240ed94f2f72d87b1d7b15549
Hzkun001 <ratihaura61@gmail.com>
2024-12-03 14:45:00 +0800

    membenarkan autoload

--- autoload.js
+++ autoload.js
@@ -22,14 +22,13 @@ function loadExternalResource(url, type) {
 	});
 }
 
-// 加载 waifu.css live2d.min.js waifu-tips.js
-if (true) {
+if (screen.width >= 768) {
 	Promise.all([
 		loadExternalResource(live2d_path + "waifu.css", "css"),
 		loadExternalResource(live2d_path + "live2d.min.js", "js"),
 		loadExternalResource(live2d_path + "waifu-tips.js", "js")
 	]).then(() => {
-		// 配置选项的具体用法见 README.md
+
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
